1d206f62cec6f3baa0c2f7393793c51e51345ab7
Kenji Saito <ken-yo@mbr.nifty.com>
2025-02-18 08:03:43 +0900

    Bumps node modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import alias from "@rollup/plugin-alias";
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import alias from '@rollup/plugin-alias';
+import { reactRouter } from '@react-router/dev/vite';
+import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     alias({
       entries: [
         { find: 'react', replacement: 'preact/compat' },
-        { find: 'react-dom', replacement: 'preact/compat' }
+        { find: 'react-dom', replacement: 'preact/compat' },
       ],
     }),
+    preact(),
     reactRouter(),
     tsconfigPaths(),
   ],
